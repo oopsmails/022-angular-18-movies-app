686c9d3d775cc50d754bbd47593231e1b4e629b3
oopsmails <oopsmails@gmail.com>
2024-07-04 10:55:02 -0400

    minor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes, withComponentInputBinding()),
+  provideHttpClient()]
 };
